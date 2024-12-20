2850dc7eb024e1e5ae26cfd60cf6dac678276b88
akash0504-2005 <sudheerakashkalam@gmail.com>
2024-12-21 00:10:49 +0530

    Create Mergesort.c

---
+++ Mergesort.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+void merge(int arr[], int left, int mid, int right) {
+    int i = left, j = mid + 1, k = 0, temp[right - left + 1];
+
+    while (i <= mid && j <= right) {
+        if (arr[i] <= arr[j])
+            temp[k++] = arr[i++];
+        else
+            temp[k++] = arr[j++];
+    }
+
+    while (i <= mid) temp[k++] = arr[i++];
+    while (j <= right) temp[k++] = arr[j++];
+
+    for (i = left, k = 0; i <= right; i++, k++)
+        arr[i] = temp[k];
+}
+
+void mergeSort(int arr[], int left, int right) {
+    if (left < right) {
+        int mid = (left + right) / 2;
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
+    }
+}
+
+int main() {
+    int arr[] = {12, 11, 13, 5, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Unsorted array: ");
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    mergeSort(arr, 0, n - 1);
+
+    printf("\nSorted array: ");
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
+
+//output
+Unsorted array:12 11 13 5 6
+Sorted array: 5 6 11 12 13
